6cfc9182efae0f26de834fb62f3d052dbeaae608
Andrés Martínez <autummata@gmail.com>
2024-10-04 19:40:21 -0600

    Update: README with structure + comments

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-## Learning Kotlin App
+# Learning Kotlin App
 An app mostly (uniquely) designed to force me to learn Kotlin. :) Made with love and Android Studio (mutually-excluyent) 
+
+> Due to the nature of the project, I will not accept any type of Pull Request unless stated otherwise.
+
+## Features (To-Do)
+- Temperature Converter 
+- Rock, Paper, Scissors!
+- 2nd Grade Equation Solver 
+
--- app/src/main/java/com/a00227463/interfacesdesarrollo_ciberseguridad/MainActivity.kt
+++ app/src/main/java/com/a00227463/interfacesdesarrollo_ciberseguridad/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.a00227463.interfacesdesarrollo_ciberseguridad
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,19 +9,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.a00227463.interfacesdesarrollo_ciberseguridad.ui.theme.InterfacesDesarrollo_CiberseguridadTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             InterfacesDesarrollo_CiberseguridadTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                // Scaffold with header and content
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text(text = "Desaarrollo de Interfaces")
+                            }
+                        )
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    GreeetingMessage(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +47,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreeetingMessage(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name! How are you?",
         modifier = modifier
     )
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     InterfacesDesarrollo_CiberseguridadTheme {
-        Greeting("Android")
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(text = "Hello World")
+                    }
+                )
+            }
+        ) {
+            GreeetingMessage("Android")
+        }
     }
 }
